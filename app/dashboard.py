7efa3ce090e2f5d440b848da8fd020b5bdf2dd0b
Anastasiia <anastasiia.nogina@gmail.com>
2026-06-16 22:27:41 +0200

    Update dashboard.py

--- app/dashboard.py
+++ app/dashboard.py
@@ -1425,7 +1425,7 @@ with tab5:
                     "Francia":      "#E2E8F0",
                 },
                 mapbox_style="carto-positron",
-                zoom=6.7,
+                zoom=7,
                 center={"lat": dept_lat, "lon": dept_lon},
                 opacity=0.75,
                 height=420,
